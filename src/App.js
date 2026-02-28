eb882bea225730e27705c1bf51561d206de95909
Nirali9726 <niraliprajapati0076@gmail.com>
2026-02-28 22:57:12 +0530

    fix routing for github pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+
 import './styles/global.css';
 // Pages
 import Opening from './pages/Opening';
